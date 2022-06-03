2a0c0d0d3b98eed9b360045e3558adbf987e8efe
IgorMoren <igor.moreno@bosonit.com>
2022-06-03 13:43:38 +0200

    Modified hero stats as 'enum'
    Creation of hero stats with an enum.
    &
    Prettier plugin doing her job.

--- src/app/libreria/libreria.component.ts
+++ src/app/libreria/libreria.component.ts
@@ -1,33 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { LibreriaService } from './services/libreria.service';
 import { Heroes } from './interfaces/heroes';
-import { ChartConfiguration, ChartData, ChartType, LinearScale, LogarithmicScale } from 'chart.js';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 
+enum heroStats {
+  hName = 'name',
+
+  hThwart = 'thwart',
+
+  hAttack = 'attack',
+
+  hDefense = 'defense',
+
+  hHand_size = 'hand_size',
+
+  hHealth = 'health',
+}
 
 @Component({
   selector: 'app-libreria',
   templateUrl: './libreria.component.html',
-  styleUrls: ['./libreria.component.sass']
+  styleUrls: ['./libreria.component.sass'],
 })
 export class LibreriaComponent implements OnInit {
-
   /*Start chart values*/
-  heroName: Heroes['name'] = '';
+  heroName: Heroes[heroStats.hName] = '';
 
-  heroThwart: Heroes['thwart'] = 0;
+  heroThwart: Heroes[heroStats.hThwart] = 0;
 
-  heroAttack: Heroes["attack"] = 0;
+  heroAttack: Heroes[heroStats.hAttack] = 0;
 
-  heroDef: Heroes['defense'] = 0;
+  heroDef: Heroes[heroStats.hDefense] = 0;
 
-  heroHand: Heroes['hand_size'] = 0;
+  heroHand: Heroes[heroStats.hHand_size] = 0;
 
-  heroHp: Heroes['health'] = 0;
- /* End chart values  */
+  heroHp: Heroes[heroStats.hHealth] = 0;
+  /* End chart values  */
 
- /* Array of heroes from marvelc cdb api */
+  /* Array of heroes from marvelc cdb api */
   heroList: Array<string> = ['Elige heroe:'];
- /* Array of heroes code from marvelc cdb api */
+  /* Array of heroes code from marvelc cdb api */
   heroCode: Array<string> = ['0'];
 
   heroListCode: any;
@@ -39,129 +51,124 @@ export class LibreriaComponent implements OnInit {
   pointBackgroundColor: string = '';
 
   heroImgString: string = '';
-  
 
-  constructor(private libreriaService: LibreriaService) { }
+  constructor(private libreriaService: LibreriaService) {}
   /* Configurando las opciones del chart, beginAtZero para que el origen se establezca en 0 y no en el valor mas pequeño:
   https://www.chartjs.org/docs/3.3.2/axes/radial/linear.html  */
   radarChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     scales: {
       r: {
-        beginAtZero: true
-      }
-      
-    }
-    
+        beginAtZero: true,
+      },
+    },
   };
 
-  radarChartLabels: string[] = ['Intervencion', 'Ataque', 'Defensa', 'Tamaño de mano', 'Vida'];
+  radarChartLabels: string[] = [
+    'Intervencion',
+    'Ataque',
+    'Defensa',
+    'Tamaño de mano',
+    'Vida',
+  ];
 
   radarChartType: ChartType = 'radar';
 
   radarChartData!: ChartData<'radar'>;
 
   ngOnInit(): void {
-
     //Almacenar el valor del name para las cartas tipo heroe con el fin de crear el option de los heroes
-    this.libreriaService.getHeroList().subscribe((heroList) => {      
+    this.libreriaService.getHeroList().subscribe((heroList) => {
       var heroLista: any;
       heroLista = Object.values(heroList);
       console.log(heroLista);
-      
+
       for (let i = 0; i < heroLista.length; i++) {
-        
         if (heroLista[i]['type_name'] === 'Hero') {
           //console.log(heroLista[i]);
           this.heroList.push(heroLista[i]['name']);
-          this.heroCode.push(heroLista[i]['code']);  
+          this.heroCode.push(heroLista[i]['code']);
         }
       }
       /* Jugar con los indices de un array y otro, coincidiran el 
       primero de uno con el primero del otro */
       //console.log(this.heroList);
-      
+
       //console.log(this.heroCode);
-    
+
       this.pintaGrafico('01001a');
-
-
-    })
+    });
   }
 
   pintaGrafico(event: any) {
-/*Evento recoge con srcElement.value el valor del option.
+    /*Evento recoge con srcElement.value el valor del option.
 Saco el indice de este y lo establezco en el indice de los codigos para obtener el 
 code de la peticion Http */
-  
-  let indexHero: any;
 
-  let indexHeroCode: any;
+    let indexHero: any;
 
-  /*Condicion if para que al cargar el componente reciba ese valor y muestre, 
+    let indexHeroCode: any;
+
+    /*Condicion if para que al cargar el componente reciba ese valor y muestre, 
   asi se evita el display de elementos vacios*/
     if (event == '01001a') {
-
       indexHeroCode = '01001a';
-
     } else {
-    
-    indexHero = this.heroList.indexOf(event.srcElement.value);
-    //console.log( this.heroCode[indexHero]);
-    
-    indexHeroCode = this.heroCode[indexHero];
+      indexHero = this.heroList.indexOf(event.srcElement.value);
+      //console.log( this.heroCode[indexHero]);
+
+      indexHeroCode = this.heroCode[indexHero];
     }
-    this.libreriaService.getPeticionHttp( indexHeroCode )
-      .subscribe((heroes) => {
-        
-        this.heroName = heroes['name'];
-        //console.log(this.heroName)
+    this.libreriaService.getPeticionHttp(indexHeroCode).subscribe((heroes) => {
+      this.heroName = heroes['name'];
+      //console.log(this.heroName)
 
-        this.heroThwart = heroes['thwart'];
-        //console.log('Intervencion:', this.heroThwart);
+      this.heroThwart = heroes['thwart'];
+      //console.log('Intervencion:', this.heroThwart);
 
-        this.heroAttack = heroes['attack'];
-        //console.log('Ataque:', this.heroAttack);
+      this.heroAttack = heroes['attack'];
+      //console.log('Ataque:', this.heroAttack);
 
-        this.heroDef = heroes['defense'];
-        //console.log('Def:', this.heroDef);
+      this.heroDef = heroes['defense'];
+      //console.log('Def:', this.heroDef);
 
-        this.heroHand = heroes['hand_size'];
-        //console.log('Mano:', this.heroHand);
+      this.heroHand = heroes['hand_size'];
+      //console.log('Mano:', this.heroHand);
 
-        this.heroHp = heroes['health'];
-        //console.log('health: ', this.heroHp);
+      this.heroHp = heroes['health'];
+      //console.log('health: ', this.heroHp);
 
-        /*Se añade al final al valor del color obtenido de la api segun: https://www.esthersola.com/transparencia-color-hexadecimal/
+      /*Se añade al final al valor del color obtenido de la api segun: https://www.esthersola.com/transparencia-color-hexadecimal/
         para añadir transparencia al codigo de color hexadecimal*/
-        this.backgroundHeroColor = `${heroes['meta']['colors']['0']}B3`;
-        //console.log('colorback: ', this.backgroundHeroColor);
+      this.backgroundHeroColor = `${heroes['meta']['colors']['0']}B3`;
+      //console.log('colorback: ', this.backgroundHeroColor);
 
-        this.backgroundHeroAlterColor = `${heroes['meta']['colors']['1']}B3`;
-        //console.log('color linea: ', this.backgroundHeroAlterColor);
+      this.backgroundHeroAlterColor = `${heroes['meta']['colors']['1']}B3`;
+      //console.log('color linea: ', this.backgroundHeroAlterColor);
 
-        this.pointBackgroundColor = heroes['meta']['colors']['2'];
+      this.pointBackgroundColor = heroes['meta']['colors']['2'];
 
-        this.heroImgString = `https://marvelcdb.com${heroes['imagesrc']}`;
-        //console.log( 'imagen', this.heroImgString);
-        
-        
-        
+      this.heroImgString = `https://marvelcdb.com${heroes['imagesrc']}`;
+      //console.log( 'imagen', this.heroImgString);
 
-        this.radarChartData = {
-          labels: this.radarChartLabels,
-          datasets: [
-            {
-              data: [this.heroThwart, this.heroAttack, this.heroDef, this.heroHand, this.heroHp],
-              label: this.heroName,
-              backgroundColor: [ this.backgroundHeroColor ],
-              borderColor: [ this.backgroundHeroAlterColor ],
-              pointBackgroundColor: [ this.pointBackgroundColor ]
-              
-            },
-          ]
-        };
-      });
+      this.radarChartData = {
+        labels: this.radarChartLabels,
+        datasets: [
+          {
+            data: [
+              this.heroThwart,
+              this.heroAttack,
+              this.heroDef,
+              this.heroHand,
+              this.heroHp,
+            ],
+            label: this.heroName,
+            backgroundColor: [this.backgroundHeroColor],
+            borderColor: [this.backgroundHeroAlterColor],
+            pointBackgroundColor: [this.pointBackgroundColor],
+          },
+        ],
+      };
+    });
   }
-
 }
